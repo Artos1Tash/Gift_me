84e124736d094ca73664bb7e0253558f547a7fd3
Artur <108674440+Kratos1top@users.noreply.github.com>
2022-09-10 15:19:51 +0600

    Update views.py

--- auth_app/views.py
+++ auth_app/views.py
@@ -7,40 +7,6 @@ from auth_app.serializer import RegisterSerializer
 
 
 
-class RegisterView(APIView):
-    # permission_classes = [permissions.AllowAny]
-
-    def post(self, request, *args, **kwargs):
-        serializer = RegisterSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        token = Token.objects.create(user=user)
-        return Response({'token': str(token.key)})
-
-
-class LoginView(APIView):
-    # permission_classes = [permissions.AllowAny]
-
-
-    def post(self, request, *args, **kwargs):
-        login = request.data.get('login')
-
-        if not User.objects.filter(username=login).exists():
-            return Response(f'{login} - does not exists')
-
-        user = User.objects.get(username=login)
-        password = request.data.get('password')
-        pass_check = check_password(password, user.password)
-
-        if not pass_check:
-            return Response('password incorrect')
-
-        token = Token.objects.get(user=user)
-        return Response({'token': str(token.key)})
-
-
-
-
 
 
 
